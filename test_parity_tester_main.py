08be742bcbe3b4048ef1a132b593e524c0ebc644
Mateusz Chudzik <matchupikchu99@gmail.com>
2023-03-22 11:04:47 +0100

    better testing

--- test_parity_tester_main.py
+++ test_parity_tester_main.py
@@ -1,7 +1,7 @@
 import random
-
 import cocotb
 from test_parity_tester import ParityTester
+from schemdraw import logic
 
 @cocotb.test()
 def test(dut):
@@ -11,9 +11,9 @@ def test(dut):
     tb.start_clock()
 
     for _ in range(10):
-        x = random.sample(range(0, 256), 10)
+        n = random.randint(2, 100)
+        x = random.sample(range(0, 256), n)
 
         yield tb.axis_s_driver._driver_send(x)
-    
     
     
